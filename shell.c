2d98d902d0ec71f9d0d91b40bad6a590759d4e41
maverickOA <your-email@example.com>
2023-11-11 23:51:53 -0800

    source code

---
+++ shell.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/*
+ *
+ */
+
+int main(void) {
+    char command[120];
+
+    while (true) {
+        show_prompt();
+        read_command(command, sizeof(command));
+        execute_command(command);
+    }
+
+    return 0;
+}
+
