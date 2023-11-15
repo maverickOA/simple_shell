c1e01a968845aacc638888cafe1021386ad13a10
maverickOA <your-email@example.com>
2023-11-15 06:46:58 -0800

    checks executable file

---
+++ _check_file.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+ * _checkfile - Check for the exisitence of the file specified at @path
+ *
+ * @path: The path to the file to be executed
+ *
+ * Return: VALID_COMMAND if @path is a valid file,
+ * NULL_FOUND if @path is NULL otherwise
+ * INVALID_COMMAND
+*/
+int _checkfile(char *path)
+{
+	if (path == NULL)
+	{
+		return (NULL_FOUND);
+	}
+	if (_isexec(path) == IS_EXEC)
+	{
+		if (access(path, F_OK) == VALID_COMMAND)
+		{
+			return (VALID_COMMAND);
+		}
+		return (INVALID_COMMAND);
+	}
+	if (_isrelpath(path) == IS_REL_PATH)
+	{
+		return (IS_REL_PATH);
+	}
+	if (access(path, F_OK) == VALID_COMMAND)
+	{
+		return (VALID_COMMAND);
+	}
+	return (INVALID_COMMAND);
+}
+
