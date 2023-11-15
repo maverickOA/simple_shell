f8bf08e40656100343c22f54410fe778f9d74ce0
maverickOA <your-email@example.com>
2023-11-15 07:20:12 -0800

    get full path

---
+++ _get_full_path.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * _getfullpath - Obtain the full PATH to @command
+ *
+ * @path: A path in the PATH environment variable
+ * @command: The given command to be executed
+ *
+ * Return: The full path to @command
+*/
+char *_getfullpath(char *path, char *command)
+{
+	return (_pathbuilder(3, path, "/", command));
+}
