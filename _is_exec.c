34d3a5a641957290c9f1b4f0d5d8d00d88d7a461
maverickOA <your-email@example.com>
2023-11-15 08:15:57 -0800

    second bulk commit

---
+++ _is_exec.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _isexec - Determines if @path is a path to
+ * an executable
+ *
+ * @path: The path to the file to be executed
+ *
+ * Return: IS_EXEC if @path is a path to an executable
+ * otherwise IS_REL_PATH
+*/
+int _isexec(char *path)
+{
+	char first_character;
+
+	first_character = path[0];
+	if (first_character == '.')
+	{
+		return (IS_EXEC);
+	}
+	return (IS_REL_PATH);
+}
+
---
+++ _strlen.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * _strlen - Return the length for given string including the
+ * NULL terminating character.
+ *
+ * @str: The given string
+ *
+ * Return: The size for the given string
+ **/
+int _strlen(char *str)
+{
+	int total_characters = 0;
+
+	while (str[total_characters] != '\0')
+	{
+		total_characters++;
+	}
+	return (total_characters + 1);
+}
+
